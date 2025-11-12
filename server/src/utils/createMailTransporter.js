7e88c8c32b29cbdd62432185b11ee20905e39df0
Aqeel <aqeelhanslo@gmail.com>
2025-11-12 15:26:02 +0200

    updated mail transporter

--- server/src/utils/createMailTransporter.js
+++ server/src/utils/createMailTransporter.js
@@ -2,13 +2,22 @@ const nodemailer = require('nodemailer')
 
 const createMailTransporter = () => {
     console.log('Creating transporter with:', process.env.HOST_EMAIL_ADDRESS);
+    // const transporter = nodemailer.createTransport({
+    //     service: 'gmail',
+    //     auth: {
+    //         user: process.env.HOST_EMAIL_ADDRESS,
+    //         pass: process.env.HOST_EMAIL_PASSWORD
+    //     }
+    // })
     const transporter = nodemailer.createTransport({
-        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587, // or 465
+        secure: false, // true for 465
         auth: {
             user: process.env.HOST_EMAIL_ADDRESS,
             pass: process.env.HOST_EMAIL_PASSWORD
         }
-    })
+    });
     return transporter
 }
 
