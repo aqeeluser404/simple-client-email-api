a472c8ba245bc0ef21bdb1ac1c608b4b6093c41a
Aqeel <aqeelhanslo@gmail.com>
2025-11-12 16:15:22 +0200

    more fixes

--- server/src/utils/createMailTransporter.js
+++ server/src/utils/createMailTransporter.js
@@ -1,24 +1,47 @@
-const nodemailer = require('nodemailer')
+// const nodemailer = require('nodemailer')
+
+// const createMailTransporter = () => {
+//     console.log('Creating transporter with:', process.env.HOST_EMAIL_ADDRESS);
+//     const transporter = nodemailer.createTransport({
+//         service: 'gmail',
+//         auth: {
+//             user: process.env.HOST_EMAIL_ADDRESS,
+//             pass: process.env.HOST_EMAIL_PASSWORD
+//         }
+//     })
+//     // const transporter = nodemailer.createTransport({
+//     //     host: 'in-v3.mailjet.com',
+//     //     port: 587,
+//     //     secure: false,
+//     //     auth: {
+//     //         user: process.env.MAILJET_API_KEY,
+//     //         pass: process.env.MAILJET_SECRET_KEY
+//     //     }
+//     // });
+//     // return transporter
+// }
+
+// module.exports = createMailTransporter
+
+const nodemailer = require('nodemailer');
 
 const createMailTransporter = () => {
-    console.log('Creating transporter with:', process.env.HOST_EMAIL_ADDRESS);
-    // const transporter = nodemailer.createTransport({
-    //     service: 'gmail',
-    //     auth: {
-    //         user: process.env.HOST_EMAIL_ADDRESS,
-    //         pass: process.env.HOST_EMAIL_PASSWORD
-    //     }
-    // })
-    const transporter = nodemailer.createTransport({
-        host: 'in-v3.mailjet.com',
-        port: 587,
-        secure: false,
-        auth: {
-            user: process.env.MAILJET_API_KEY,
-            pass: process.env.MAILJET_SECRET_KEY
-        }
-    });
-    return transporter
-}
+    console.log('Creating Gmail transporter (Port 465) for:', process.env.HOST_EMAIL_ADDRESS);
 
-module.exports = createMailTransporter
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true, // ← SSL, not STARTTLS
+        auth: {
+            user: process.env.HOST_EMAIL_ADDRESS,
+            pass: process.env.HOST_EMAIL_PASSWORD
+        },
+        connectionTimeout: 10000, // 10 seconds
+        greetingTimeout: 10000,
+        socketTimeout: 10000
+    });
+
+    return transporter;
+};
+
+module.exports = createMailTransporter;
