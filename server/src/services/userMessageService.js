ffb9776f03a1e10a03f04bd680d3641d20114628
Aqeel <aqeelhanslo@gmail.com>
2026-03-11 11:54:15 +0200

    Refactor SendMessageService to conditionally include phoneNumber in UserMessageData

--- server/src/services/userMessageService.js
+++ server/src/services/userMessageService.js
@@ -2,16 +2,20 @@ const getInContactEmail = require('../utils/sendEmail');
 console.log('SendMessageService.js loaded');
 
 module.exports.SendMessageService = async (message) => {
-    try {
-        const UserMessageData = {
-            name: message.name,
-            email: message.email,
-            phoneNumber: message.phoneNumber,
-            message: message.message,
-            dateCreated: new Date()
-        };
-        await getInContactEmail(UserMessageData)
-    } catch (error) {
-        throw error
+  try {
+    const UserMessageData = {
+      name: message.name,
+      email: message.email,
+      message: message.message,
+      dateCreated: new Date()
+    };
+
+    if (message.phoneNumber) {
+      UserMessageData.phoneNumber = message.phoneNumber;
     }
-}
+
+    await getInContactEmail(UserMessageData);
+  } catch (error) {
+    throw error;
+  }
+};
